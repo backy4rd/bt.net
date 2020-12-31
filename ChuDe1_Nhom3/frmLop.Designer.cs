c592f47578ee4ff60f88a6e011a9789f3ff0ecb9
hnyuhcognel <lengochuynh.tt@gmail.com>
2020-12-31 09:26:26 +0700

    form Lop

--- ChuDe1_Nhom3/frmLop.Designer.cs
+++ ChuDe1_Nhom3/frmLop.Designer.cs
@@ -28,120 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgvLophoc = new System.Windows.Forms.DataGridView();
+            this.btnDongform = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnChinhsua = new System.Windows.Forms.Button();
+            this.btnKhongluu = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtTenlop = new System.Windows.Forms.TextBox();
+            this.txtMalop = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cbTrungtam = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLophoc)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvLophoc
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(31, 196);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(386, 153);
-            this.dataGridView1.TabIndex = 31;
+            this.dgvLophoc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLophoc.Location = new System.Drawing.Point(12, 196);
+            this.dgvLophoc.Name = "dgvLophoc";
+            this.dgvLophoc.Size = new System.Drawing.Size(526, 153);
+            this.dgvLophoc.TabIndex = 31;
+            this.dgvLophoc.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLophoc_CellClick);
             // 
-            // button10
+            // btnDongform
             // 
-            this.button10.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(205, 406);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(122, 32);
-            this.button10.TabIndex = 30;
-            this.button10.Text = "Đóng Form";
-            this.button10.UseVisualStyleBackColor = true;
+            this.btnDongform.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDongform.Location = new System.Drawing.Point(262, 409);
+            this.btnDongform.Name = "btnDongform";
+            this.btnDongform.Size = new System.Drawing.Size(122, 32);
+            this.btnDongform.TabIndex = 30;
+            this.btnDongform.Text = "Đóng Form";
+            this.btnDongform.UseVisualStyleBackColor = true;
+            this.btnDongform.Click += new System.EventHandler(this.btnDongform_Click);
             // 
-            // button9
+            // btnXoa
             // 
-            this.button9.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(127, 406);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(72, 32);
-            this.button9.TabIndex = 29;
-            this.button9.Text = "Xóa";
-            this.button9.UseVisualStyleBackColor = true;
+            this.btnXoa.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(184, 409);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(72, 32);
+            this.btnXoa.TabIndex = 29;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
-            // button8
+            // btnLuu
             // 
-            this.button8.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(227, 368);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(72, 32);
-            this.button8.TabIndex = 28;
-            this.button8.Text = "Lưu";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btnLuu.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(284, 371);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(72, 32);
+            this.btnLuu.TabIndex = 28;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
             // 
-            // button7
+            // btnChinhsua
             // 
-            this.button7.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(109, 368);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(112, 32);
-            this.button7.TabIndex = 26;
-            this.button7.Text = "Chỉnh sửa";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnChinhsua.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChinhsua.Location = new System.Drawing.Point(166, 371);
+            this.btnChinhsua.Name = "btnChinhsua";
+            this.btnChinhsua.Size = new System.Drawing.Size(112, 32);
+            this.btnChinhsua.TabIndex = 26;
+            this.btnChinhsua.Text = "Chỉnh sửa";
+            this.btnChinhsua.UseVisualStyleBackColor = true;
+            this.btnChinhsua.Click += new System.EventHandler(this.btnChinhsua_Click);
             // 
-            // button6
+            // btnKhongluu
             // 
-            this.button6.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(305, 368);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(112, 32);
-            this.button6.TabIndex = 27;
-            this.button6.Text = "Không lưu";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnKhongluu.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhongluu.Location = new System.Drawing.Point(362, 371);
+            this.btnKhongluu.Name = "btnKhongluu";
+            this.btnKhongluu.Size = new System.Drawing.Size(112, 32);
+            this.btnKhongluu.TabIndex = 27;
+            this.btnKhongluu.Text = "Không lưu";
+            this.btnKhongluu.UseVisualStyleBackColor = true;
+            this.btnKhongluu.Click += new System.EventHandler(this.btnKhongluu_Click);
             // 
-            // button5
+            // btnThem
             // 
-            this.button5.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(31, 368);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(72, 32);
-            this.button5.TabIndex = 25;
-            this.button5.Text = "Thêm";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnThem.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(88, 371);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(72, 32);
+            this.btnThem.TabIndex = 25;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
-            // textBox3
+            // txtTenlop
             // 
-            this.textBox3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(161, 104);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(256, 30);
-            this.textBox3.TabIndex = 24;
+            this.txtTenlop.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenlop.Location = new System.Drawing.Point(161, 104);
+            this.txtTenlop.Name = "txtTenlop";
+            this.txtTenlop.Size = new System.Drawing.Size(377, 30);
+            this.txtTenlop.TabIndex = 24;
             // 
-            // textBox2
+            // txtMalop
             // 
-            this.textBox2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(161, 150);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(256, 30);
-            this.textBox2.TabIndex = 23;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(161, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(256, 30);
-            this.textBox1.TabIndex = 22;
+            this.txtMalop.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMalop.Location = new System.Drawing.Point(161, 59);
+            this.txtMalop.Name = "txtMalop";
+            this.txtMalop.Size = new System.Drawing.Size(377, 30);
+            this.txtMalop.TabIndex = 22;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(27, 111);
+            this.label4.Location = new System.Drawing.Point(12, 111);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(120, 23);
             this.label4.TabIndex = 21;
@@ -151,17 +150,17 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(27, 157);
+            this.label3.Location = new System.Drawing.Point(17, 157);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(133, 23);
+            this.label3.Size = new System.Drawing.Size(104, 23);
             this.label3.TabIndex = 20;
-            this.label3.Text = "Mã trung tâm:";
+            this.label3.Text = "Trung tâm:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 66);
+            this.label2.Location = new System.Drawing.Point(12, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(115, 23);
             this.label2.TabIndex = 19;
@@ -171,27 +170,37 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(105, 9);
+            this.label1.Location = new System.Drawing.Point(180, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(238, 32);
             this.label1.TabIndex = 18;
             this.label1.Text = "Danh mục lớp học";
             // 
+            // cbTrungtam
+            // 
+            this.cbTrungtam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTrungtam.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTrungtam.FormattingEnabled = true;
+            this.cbTrungtam.Location = new System.Drawing.Point(161, 149);
+            this.cbTrungtam.Name = "cbTrungtam";
+            this.cbTrungtam.Size = new System.Drawing.Size(377, 31);
+            this.cbTrungtam.TabIndex = 32;
+            // 
             // frmLop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 458);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(550, 458);
+            this.Controls.Add(this.cbTrungtam);
+            this.Controls.Add(this.dgvLophoc);
+            this.Controls.Add(this.btnDongform);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnChinhsua);
+            this.Controls.Add(this.btnKhongluu);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTenlop);
+            this.Controls.Add(this.txtMalop);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -199,7 +208,8 @@
             this.Name = "frmLop";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thông tin lớp học";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmLop_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLophoc)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,19 +217,19 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgvLophoc;
+        private System.Windows.Forms.Button btnDongform;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnChinhsua;
+        private System.Windows.Forms.Button btnKhongluu;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtTenlop;
+        private System.Windows.Forms.TextBox txtMalop;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbTrungtam;
     }
 }
--- ChuDe1_Nhom3/frmLop.cs
+++ ChuDe1_Nhom3/frmLop.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,215 @@ namespace ChuDe1_Nhom3
 {
     public partial class frmLop : Form
     {
+        DataSet lop = new DataSet();
+        DataSet trungtam = new DataSet();
+        bool blnThem = false;
+
         public frmLop()
         {
             InitializeComponent();
         }
+
+        void DieuKhienKhiBinhThuong()
+        {
+            if (MyPublic.quyenSD == "AdTinh" || MyPublic.quyenSD == "AdTruong")
+            {
+                btnThem.Enabled = true;
+                btnChinhsua.Enabled = true;
+                btnXoa.Enabled = true;
+            }
+            else
+            {
+                btnThem.Enabled = false;
+                btnChinhsua.Enabled = false;
+                btnXoa.Enabled = false;
+            }
+            btnLuu.Enabled = false;
+            btnKhongluu.Enabled = false;
+            btnDongform.Enabled = true;
+            txtMalop.ReadOnly = true;
+            txtTenlop.ReadOnly = true;
+        }
+
+        void DieuKhienKhiThem()
+        {
+            btnThem.Enabled = false;
+            btnChinhsua.Enabled = false;
+            btnLuu.Enabled = true;
+            btnKhongluu.Enabled = true;
+            btnXoa.Enabled = false;
+            btnDongform.Enabled = false;
+            txtMalop.ReadOnly = false;
+            txtTenlop.ReadOnly = false;
+            txtMalop.Clear();
+            txtTenlop.Clear();
+            cbTrungtam.SelectedIndex = -1;
+            txtMalop.Select();
+        }
+
+        void DieuKhienKhiChinhSua()
+        {
+            btnThem.Enabled = false;
+            btnChinhsua.Enabled = false;
+            btnLuu.Enabled = true;
+            btnKhongluu.Enabled = true;
+            btnXoa.Enabled = false;
+            btnDongform.Enabled = false;
+            txtMalop.ReadOnly = true;
+            txtTenlop.ReadOnly = false;
+            txtMalop.Select();
+        }
+
+        void GanDuLieu()
+        {
+            if (lop.Tables["LopHoc"].Rows.Count > 0)
+            {
+                txtMalop.Text = dgvLophoc[0, dgvLophoc.CurrentRow.Index].Value.ToString();
+                txtTenlop.Text = dgvLophoc[1, dgvLophoc.CurrentRow.Index].Value.ToString();
+                cbTrungtam.SelectedValue = dgvLophoc[2, dgvLophoc.CurrentRow.Index].Value.ToString();
+            }
+            else
+            {
+                txtMalop.Clear();
+                txtTenlop.Clear();
+                cbTrungtam.SelectedIndex = -1;
+            }
+        }
+
+
+        private void btnDongform_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmLop_Load(object sender, EventArgs e)
+        {
+            string strSelect = "Select * From LopHoc";
+            MyPublic.OpenData(strSelect, lop, "LopHoc");
+
+            strSelect = "Select MaTT, TenTT From TrungTam";
+            MyPublic.OpenData(strSelect, trungtam, "TrungTam");
+
+            cbTrungtam.DataSource = trungtam.Tables["TrungTam"];
+            cbTrungtam.DisplayMember = "TenTT";
+            cbTrungtam.ValueMember = "MaTT";
+
+
+            txtMalop.MaxLength = 8;
+            txtTenlop.MaxLength = 40;
+
+            dgvLophoc.DataSource = lop;
+            dgvLophoc.DataMember = "LopHoc";
+            dgvLophoc.Width = 526;
+            dgvLophoc.AllowUserToAddRows = false;
+            dgvLophoc.AllowUserToDeleteRows = false;
+            dgvLophoc.Columns[0].Width = 100;
+            dgvLophoc.Columns[0].HeaderText = "Mã lớp";
+            dgvLophoc.Columns[1].Width = 265;
+            dgvLophoc.Columns[1].HeaderText = "Tên lớp";
+            dgvLophoc.Columns[2].Width = 100;
+            dgvLophoc.Columns[2].HeaderText = "Mã trung tâm";
+            dgvLophoc.EditMode = DataGridViewEditMode.EditProgrammatically;
+            GanDuLieu();
+            DieuKhienKhiBinhThuong();
+        }
+
+        private void dgvLophoc_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            GanDuLieu();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            blnThem = true;
+            DieuKhienKhiThem();
+        }
+
+        private void btnChinhsua_Click(object sender, EventArgs e)
+        {
+            DieuKhienKhiChinhSua();
+        }
+
+        void Luu()
+        {
+            string sql, matt;
+            if (blnThem)
+                sql = "Insert Into LopHoc Values(@MaLop, @TenLop, @MaTT)";
+            else
+                sql = "Update LopHoc Set TenLop = @TenLop, MaTT = @MaTT Where MaLop = @MaLop"; 
+            if (MyPublic.connection.State == ConnectionState.Closed)
+                MyPublic.connection.Open();
+            SqlCommand command = new SqlCommand(sql, MyPublic.connection);
+            command.Parameters.AddWithValue("@MaLop", txtMalop.Text);
+            command.Parameters.AddWithValue("@TenLop", txtTenlop.Text);
+            matt = cbTrungtam.SelectedValue.ToString();
+            command.Parameters.AddWithValue("@MaTT", matt);
+           
+            command.ExecuteNonQuery();
+            MyPublic.connection.Close();
+            if (blnThem)
+            {
+                lop.Tables["LopHoc"].Rows.Add(txtMalop.Text, txtTenlop.Text, matt);
+                GanDuLieu();
+                blnThem = false;
+            }
+            else
+            {
+                int curRow = dgvLophoc.CurrentRow.Index;
+                lop.Tables["LopHoc"].Rows[curRow][0] = txtMalop.Text;
+                lop.Tables["LopHoc"].Rows[curRow][1] = txtTenlop.Text;
+                lop.Tables["LopHoc"].Rows[curRow][2] = matt;
+            }
+            DieuKhienKhiBinhThuong();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+             if ((txtMalop.Text == "") || (txtTenlop.Text == "") || (cbTrungtam.SelectedIndex == -1))
+                MessageBox.Show("Lỗi nhập dữ liệu !");
+            else
+                 if ((blnThem) && (MyPublic.tonTaiKhoaChinh(txtMalop.Text, "MaLop", "LopHoc")))
+                {
+                    MessageBox.Show("Mã số lớp này đã có rồi !");
+                    txtMalop.Focus();
+                }
+                else
+                    Luu();
+        }
+
+        private void btnKhongluu_Click(object sender, EventArgs e)
+        {
+            blnThem = false;
+            GanDuLieu();
+            DieuKhienKhiBinhThuong();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MyPublic.tonTaiKhoaChinh(txtMalop.Text, "MaLop", "LopHoc"))
+                MessageBox.Show("Phải xóa bài thi của lớp trước !");
+            else
+            {
+                DialogResult dongY;
+                dongY = MessageBox.Show("Bạn thật sự muốn xóa ?", "Xác Nhận", MessageBoxButtons.YesNo);
+                if (dongY == DialogResult.Yes)
+                {
+                    string delete = "Delete LopHoc Where MaLop = @MaLop";
+                    if (MyPublic.connection.State == ConnectionState.Closed)
+                        MyPublic.connection.Open();
+                    SqlCommand cmdCommand = new SqlCommand(delete, MyPublic.connection);
+                    cmdCommand.Parameters.AddWithValue("@MaLop", txtMalop.Text);
+                    cmdCommand.ExecuteNonQuery();
+                    MyPublic.connection.Close();
+                    int curRow = dgvLophoc.CurrentRow.Index;
+                    lop.Tables["LopHoc"].Rows.RemoveAt(curRow);
+                    GanDuLieu();
+                }
+            }
+            DieuKhienKhiBinhThuong();
+        }
+
+
+
     }
 }
